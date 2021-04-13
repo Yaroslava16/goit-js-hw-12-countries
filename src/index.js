f259f42d7456c7809988252c62ae92adb24008f9
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-04-13 22:48:01 +0300

    starts working with input

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './styles.css';
-import fetchCountries from './js/fetch-countries';
+// import fetchCountries from './js/fetch-countries';
+import * as _ from 'lodash';
+
+const input = document.querySelector('#country-name-input');
+
+input.addEventListener('input', _.debounce(onInputClick, 500));
+
+function onInputClick(e) {
+  console.log(input.value);
+}
