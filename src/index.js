869b5ff7a74193e128cdce740dbfa91e0580681a
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-04-14 22:07:23 +0300

    working with data from one country

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import './styles.css';
-// import fetchCountries from './js/fetch-countries';
+import API from './js/api-service';
+import getRefs from './js/get-refs';
 import * as _ from 'lodash';
+import countryCardTpl from './templates/country-data.hbs';
 
-const input = document.querySelector('#country-name-input');
+const refs = getRefs();
 
-input.addEventListener('input', _.debounce(onInputClick, 500));
+refs.input.addEventListener('input', _.debounce(onInputClick, 500));
 
 function onInputClick(e) {
-  console.log(input.value);
+  e.preventDefault();
+
+  const searchQuery = e.target.value;
+
+  API.fetchCountries(searchQuery)
+    .then(renderCountryCard)
+    .catch(onFetchError)
+    .finally(() => (refs.input.value = ''));
+
+  // if (refs.input.value === { name }) {
+  //   fetchCountries(searchQuery);
+  // }
+}
+
+function renderCountryCard(country) {
+  const markup = countryCardTpl(country);
+  refs.cardConteiner.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert('Такой страны не существует :(');
 }
